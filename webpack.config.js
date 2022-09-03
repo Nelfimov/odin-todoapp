7b2b1875a53afe4b4d24f19c2552460233d6f454
nelfimov <nelfimov@mail.ru>
2022-09-03 12:15:07 +0300

    add fonts, icons and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,27 @@
-webpack.config.js
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ],
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  }
+}
